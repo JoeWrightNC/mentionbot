e8838374e10ed744db7156e1e53cc6edf86f8e00
Joe Wright <joe@statesnewsroom.com>
2019-11-07 11:28:37 -0500

    here we go woooo

--- src/server.js
+++ src/server.js
@@ -176,7 +176,7 @@ feedparser.on('readable', function () {
 app.post('/', (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
-    var responseURI = req.body.payload;
+    var responseURI = req.body.payload.type;
     console.log(responseURI);
     /* request.post(res.payload.message.response_url, {
         json: {
