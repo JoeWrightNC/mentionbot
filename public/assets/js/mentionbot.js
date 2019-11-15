f963f3837791078a37c1171920979008d53088b0
Joe Wright <joe@statesnewsroom.com>
2019-11-15 14:43:09 -0500

    hey

---
+++ public/assets/js/mentionbot.js
@@ -0,0 +1,19 @@
+$("#Mention").on("click", function() {
+    $(".mentionCard").hide();
+    $(".Mention").show()
+})
+
+$("#Reprint").on("click", function() {
+    $(".mentionCard").hide();
+    $(".Reprint").show()
+})
+
+$("#Bigtime").on("click", function() {
+    $(".mentionCard").hide();
+    $(".Bigtime").show()
+})
+
+$("#Cringeworthy").on("click", function() {
+    $(".mentionCard").hide();
+    $(".Cringeworthy").show()
+})
