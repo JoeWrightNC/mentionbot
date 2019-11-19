4bf7c633337598bee8c74c2c10fbc1097388a4d3
Joe Wright <joe@statesnewsroom.com>
2019-11-19 14:36:47 -0500

    easter egg

--- public/assets/js/mentionbot.js
+++ public/assets/js/mentionbot.js
@@ -22,4 +22,21 @@ $( document ).ready(function() {
         $(".mentionCard").hide();
         $(".Cringeworthy").show()
     })
+    $("#pokeBot").on("click", function() {
+        console.log("clickety Clack")
+        $(".pokeText").text("")
+        typeWriter();
+    })
 });
+
+var i = 0;
+var txt = "Ommmmmm, Did you just poke me?"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    $(".pokeText").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
