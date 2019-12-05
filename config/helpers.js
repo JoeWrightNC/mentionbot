a6f4431f7131dcdb2fbfda311d5111589a520ef1
Joe Wright <joe@statesnewsroom.com>
2019-12-05 11:53:54 -0500

    hi

--- config/helpers.js
+++ config/helpers.js
@@ -3,6 +3,11 @@ module.exports = {
         return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
     },
     ifEqualsTwo: function(arg1, arg2, arg3, options) {
+      console.log("================================================")
+      console.log(arg1);
+      console.log(arg2);
+      console.log(arg3);
+      console.log("================================================")
         return (arg1 === arg2 || arg3) ? options.fn(this) : options.inverse(this);
     },
     bar: function(){
