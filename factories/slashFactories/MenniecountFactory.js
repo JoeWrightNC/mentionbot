088bbaf420f0a1424f6d57cac2473d230c5987b8
Joe Wright <joe@statesnewsroom.com>
2019-12-19 11:34:24 -0500

    currCount

--- factories/slashFactories/MenniecountFactory.js
+++ factories/slashFactories/MenniecountFactory.js
@@ -6,7 +6,7 @@ const MenniecountFactory = () => (body) => new Promise((resolve, reject) => {
     var selectStatement =`SELECT COUNT(*) FROM mentions;`
 
     connection.query(selectStatement, function(err, rows, fields) {
-        var currCount = rows;
+        var currCount = rows[0]['COUNT(*)'];
         console.log(currCount);
         return resolve({
             text: `Hey There!  Since I started counting, you've saved ${currCount} press mentions!  Wow, that's a lot! `,
