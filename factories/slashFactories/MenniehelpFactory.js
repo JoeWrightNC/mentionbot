b042766237031ea138b34e6c39425a1512adf949
Joe Wright <joe@statesnewsroom.com>
2019-12-19 15:58:02 -0500

    hey

--- factories/slashFactories/MenniehelpFactory.js
+++ factories/slashFactories/MenniehelpFactory.js
@@ -1,7 +1,7 @@
 const MenniehelpFactory = () => (body) => new Promise((resolve, reject) => {
     
     return resolve({
-      text: `Hey There!  I'm Mennie the MentionBot :mennie:, and I'm here to help you better track press mentions! \n\n-To view the total press mentions saved since I started counting, type */Menniecount* . \n-To hear a funny journalism joke, type */Menniejoke* `,
+      text: `Hey There!  I'm Mennie the MentionBot :mennie:, and I'm here to help you better track press mentions! \n\n-To view the total press mentions saved since I started counting, type */Menniecount* . \n-To hear a funny journalism joke, type */Menniejoke* \n\n-Looking for the mention dashboard?  You can find it at: https://sn-mentionbot.herokuapp.com/ `,
     })
 })
 
