114024463ec05eb93a77dd4be64373616401d5f4
Joe Wright <joe@statesnewsroom.com>
2020-01-02 08:52:11 -0500

    check logs for new error

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.listen(port, () => {
     console.log(`Server started at localhost:${port}`)
 })
 
-/* web.chat.postMessage({
+/*  web.chat.postMessage({
     channel: 'mentionbot',
     text: "HI!  It's me, Mennie, just letting you know Joe just pushed up code and I accepted it into my skill sets.  Have a great day and thanks for the fresh code!"
 })    */
