5c1486328b1581a47fa2e57c018eca05c686fa2d
Joe Wright <joe@statesnewsroom.com>
2020-02-10 14:00:56 -0500

    hi

--- config/helpers.js
+++ config/helpers.js
@@ -6,9 +6,13 @@ module.exports = {
       return "BAR!";
     },
     domClean: function(dom) {
+      console.log(dom);
       cleanerOne = dom.split('&url=')[1];
+      console.log(cleanerOne);
       cleanerTwo = cleanerOne.split('.com')[0];
+      console.log(cleanerTwo);
       cleanerThree = cleanerTwo.split('.org')[0];
+      console.log(cleanerThree);
       return cleanerThree;
     }
 }
